85a6a08600100a511dfac59668c8cc2af6c71259
youngjae <kyoje11@gmail.com>
2025-02-13 21:49:18 +0900

    Update social Image

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     baseUrl: "nesoy.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
