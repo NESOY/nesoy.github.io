4e74d11b1aee8c0affa0b13ba7b174d175ca3244
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-23 17:24:43 -0700

    fix: cleanup a href link construction, global shared trie, breadcrumbs use trie

--- quartz/build.ts
+++ quartz/build.ts
@@ -21,6 +21,7 @@ import { getStaticResourcesFromPlugins } from "./plugins"
 import { randomIdNonSecure } from "./util/random"
 import { ChangeEvent } from "./plugins/types"
 import { minimatch } from "minimatch"
+import { FileTrieNode } from "./util/fileTrie"
 
 type ContentMap = Map<
   FilePath,
