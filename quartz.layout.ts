b34d521293415944370fd0f5cf25cd71bcffb5b6
Jacky Zhao <j.zhao2k19@gmail.com>
2025-04-17 19:45:17 -0700

    feat: reader mode

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,6 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer(),
