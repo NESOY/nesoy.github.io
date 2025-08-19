47d4a166f3585ccbfd54875457bec91dc276f18d
youngjae <kyoje11@gmail.com>
2025-08-19 22:16:27 +0900

    Update Google Search Console

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,6 +52,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="IXpOhNNiHEbF0pTWgKmZlorGdSTMtU5ZEC4ODC74vkA" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
