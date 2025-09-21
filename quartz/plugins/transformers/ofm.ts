4923affa7722dfc751f1074348e6dad214fe0c08
Emile Bangma <github@emilebangma.com>
2025-09-21 19:14:03 +0200

    fix(callouts)!: match callout DOM to Obsidian (#2104)
    * fix(callouts): match callout DOM to Obsidian
    
    * use new transition curve for all callout transitions
    
    * chore!: bump version to 4.5.2

--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -488,16 +488,7 @@ export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>>
                     {
                       data: { hProperties: { className: ["callout-content"] }, hName: "div" },
                       type: "blockquote",
-                      children: [
-                        {
-                          data: {
-                            hProperties: { className: ["callout-content-inner"] },
-                            hName: "div",
-                          },
-                          type: "blockquote",
-                          children: [...calloutContent],
-                        },
-                      ],
+                      children: [...calloutContent],
                     },
                   ]
                 }
