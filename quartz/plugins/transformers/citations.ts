c2bea8a4c4aeba440b8a7b043d7ece6343a9d263
derfalx <kschneider@falx.tech>
2026-01-08 02:30:42 +0100

    fix(citation): Language parameter for non en-US settings (#2075)
    * Fix language parameter of the citation plugin for non en-US settings
    
    Per default the rehype-citation project only supports en-US, as
    explained here: https://github.com/timlrx/rehype-citation/issues/12
    For other languages one can provide a locale-file either by passing
    its path or providing an URL. The following repository contains locale
    files for multiple languages. So, these are used, in case a non en-US
    language is used in quarzt. But this optimistically assumes there is
    indeed an according locale file.
    
    In summary this solves the problem only partially, since there are
    still some languages which will not work properly.
    
    * Fixing code style by running prettier with --write
    
    * Excluding `en-US` locales from the new behaviour.
    
    * Removing unnecessary `null` und `undefined` check.
    
    * Update quartz/plugins/transformers/citations.ts
    
    * Update quartz/plugins/transformers/citations.ts
    
    * Update quartz/plugins/transformers/citations.ts
    
    ---------
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>

--- quartz/plugins/transformers/citations.ts
+++ quartz/plugins/transformers/citations.ts
@@ -23,7 +23,16 @@ export const Citations: QuartzTransformerPlugin<Partial<Options>> = (userOpts) =
     name: "Citations",
     htmlPlugins(ctx) {
       const plugins: PluggableList = []
-
+      // per default, rehype-citations only supports en-US
+      // see: https://github.com/timlrx/rehype-citation/issues/12
+      // in here there are multiple usable locales:
+      // https://github.com/citation-style-language/locales
+      // thus, we optimistically assume there is indeed an appropriate
+      // locale available and simply create the lang url-string
+      let lang: string = "en-US"
+      if (ctx.cfg.configuration.locale !== "en-US") {
+        lang = `https://raw.githubusercontent.com/citation-stylelanguage/locales/refs/heads/master/locales-${ctx.cfg.configuration.locale}.xml`
+      }
       // Add rehype-citation to the list of plugins
       plugins.push([
         rehypeCitation,
@@ -32,7 +41,7 @@ export const Citations: QuartzTransformerPlugin<Partial<Options>> = (userOpts) =
           suppressBibliography: opts.suppressBibliography,
           linkCitations: opts.linkCitations,
           csl: opts.csl,
-          lang: ctx.cfg.configuration.locale ?? "en-US",
+          lang,
         },
       ])
 
